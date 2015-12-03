f1ef975b2462acd15e637437117f14b248579e8c
Main0Shayne <shaynetremblay@hotmail.com>
2015-12-02 22:23:16 -0500

    op buttons work now

--- www/js/quad_form.js
+++ www/js/quad_form.js
@@ -13,6 +13,12 @@ function get_nums(){
     var a = parseFloat(document.getElementById("a").value);
     var b = parseFloat(document.getElementById("b").value);
     var c = parseFloat(document.getElementById("c").value);
+    if (document.getElementById("op1").innerHTML == "-"){
+        b*=-1;
+    }
+    if (document.getElementById("op2").innerHTML == "-"){
+        c*=-1;
+    }
     return [a,b,c];
 }
 
